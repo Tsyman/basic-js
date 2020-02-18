4d25b693f1127b1c6883e9a648e0ace3e9a64a04
Max Tsyman <xxxmaximxxx777@gmail.com>
2020-02-18 15:55:51 +0300

    feat: add carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,20 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+   if (typeof sampleActivity !== 'string') {
+    return false;
+  }
+
+  let num = parseFloat(sampleActivity);
+
+  if (sampleActivity <= 0 || sampleActivity > MODERN_ACTIVITY || !num) {
+    return false;
+  }
+  
+  let diffActivity =  MODERN_ACTIVITY / num;
+  let rateConst = 0.693 / HALF_LIFE_PERIOD;
+  let ln = Math.log(diffActivity);
+  let age = Math.ceil(ln / rateConst);
+  return age;
 };
