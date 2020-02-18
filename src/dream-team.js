84c51b5b4c6f5ec85a1a6232db6843fddfd93398
Max Tsyman <xxxmaximxxx777@gmail.com>
2020-02-18 18:10:27 +0300

    feat: add dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,17 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  let team = [];
+  
+  for (let i = 0; i < members.length; i++) {
+    if (typeof members[i] == "string") {
+       team.push(members[i].trim());
+    }
+  }
+
+  let str = team.map( word => word[0].toUpperCase());
+  let sortNames = str.sort();
+  let dreamTeam = sortNames.join().toUpperCase().replace(/,/g, "");
+  return dreamTeam; 
 };
