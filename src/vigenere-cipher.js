88234f7cb209acddac2f12be40a7f729d309d171
Max Tsyman <xxxmaximxxx777@gmail.com>
2020-02-22 21:48:50 +0300

    feat: add vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,68 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor(value = true) {
+        this.direct = value;
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    encrypt(message, key) {
+        if ( !message || !key ) {
+            throw new Error("error");
+        }
+
+        message = message.toUpperCase();
+        key = key.toUpperCase();
+        
+        const offset = "A".charCodeAt(0);
+        const alphabetSize = 26;
+
+        let result = "";
+        let keyIndex = 0;
+        for ( let i = 0; i < message.length; i++) {
+            // if character not [A-Z] don't take this character
+            if ( message.charCodeAt(i) < offset || message.charCodeAt(i) > offset + alphabetSize ) {
+                result += message[i];
+                continue;
+            } else {
+                let index = ((message.charCodeAt(i) - offset + key.charCodeAt(keyIndex) - offset) % alphabetSize) + offset;
+                result += String.fromCharCode(index);
+            }
+
+            keyIndex++;
+            if(keyIndex == key.length){
+                keyIndex = 0;
+            }
+           
+        }
+        return this.direct ? result : result.split('').reverse().join('');
+    }
+
+    decrypt(message, key) {
+        if ( !message || !key ) {
+            throw new Error("error");
+        }
+
+        message = message.toUpperCase();
+        key = key.toUpperCase();
+        const offset = "A".charCodeAt(0);
+        const alphabetSize = 26;
+
+        let result = "";
+        let keyIndex = 0;
+
+        for ( let i = 0; i < message.length; i++) { 
+            // if character not [A-Z] don't take this character
+            if ( message.charCodeAt(i) < offset || message.charCodeAt(i) > offset + alphabetSize ) {
+                result += message[i];
+                continue;
+            } else {
+                let index = (((message.charCodeAt(i) - offset) + alphabetSize - (key.charCodeAt(keyIndex) - offset)) % alphabetSize) + offset
+                result += String.fromCharCode(index);
+            }
+            keyIndex++;
+            if(keyIndex == key.length){
+                keyIndex = 0;
+            }
+        }
+        return this.direct ? result : result.split('').reverse().join('');
     }
 }
 
